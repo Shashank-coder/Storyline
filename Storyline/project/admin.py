a381a0ae9250358c3382f17a9aacc17a7ea95905
Shashank <shivam.shashank1@gmail.com>
2016-04-02 16:49:53 +0530

    models registered

--- Storyline/project/admin.py
+++ Storyline/project/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import story, chapter, comments
 
 # Register your models here.
+admin.site.register(story)
+admin.site.register(chapter)
+admin.site.register(comments)
